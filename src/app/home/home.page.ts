bd8cba045e0c20d670e72e309fe21dc3371f1239
Koushik <askoushik4@gmail.com>
2019-12-09 12:04:31 +0530

    Formatted a the typescript page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,32 +13,73 @@ export class HomePage implements OnInit, AfterContentInit {
 postionOptions: any;
 
     map;
-    markers;
+    currentPosition;
 
   @ViewChild('mapElement', { static: true}) mapElement;
 
   constructor(private ngZone: NgZone,private geolocation: Geolocation) {}
 
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {  }
 
   ngAfterContentInit(): void {
-
-      this.loadMap()
-            // this.errorLog,
-      // this.postionOptions);
-
+      this.getCurrentLocation()
    }
 
-   errorLog(error): void {
+  getCurrentLocation() {
+    const postionOptions = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    this.geolocation.getCurrentPosition(postionOptions)
+     .then((pos) => {
+       console.log('Successfull');
+       this.currentPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+       this.loadMap();
+     })
+     .catch((error) => {
        console.log('Error getting location', error);
+     });
+  }
+
+  loadMap(): void {
+    const mapOptions = {
+      center: this.currentPosition,
+      zoom: 50,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    console.log(this.currentPosition);
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker(this.currentPosition);
+    // this.nearbyPlace(this.currentPosition);
+    this.getPositionsOfBusiness();
    }
 
+  addMarker(latLng,): void {
+    const marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+      icon: {
+        url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      }
+      });
+
+    marker.setMap(this.map);
+  }
+
+  getPositionsOfBusiness(){
+    const markers = [
+      {
+        lat:12.9569,
+        lng:77.7011
+      }
+    ];
+    this.createMarker(markers[0])
+  }
    nearbyPlace(latLng) {
-       // this.loadMap();
-       this.markers = [];
+
        const service = new google.maps.places.PlacesService(this.map);
 
       console.log(service);
@@ -53,80 +94,23 @@ postionOptions: any;
      callback(results, status) {
        if (status === google.maps.places.PlacesServiceStatus.OK) {
          for (var i = 0; i < results.length; i++) {
-           this.createMarker(results[i]);
+           this.createMarker(results[i].geometry.location);
          }
        }
      }
 
      createMarker(place) {
-       const placeLoc = place;
-       console.log('placeLoc', placeLoc)
+
        this.markers = new google.maps.Marker({
            map: this.map,
-           position: place.geometry.location
+           position: place
        });
        // console.log()
-       const infowindow = new google.maps.InfoWindow();
-       google.maps.event.addListener(this.markers, 'click', () => {
-         this.ngZone.run(() => {
-           infowindow.setContent(place.name);
-           infowindow.open(this.map, this.markers);
-         });
-       });
-     }
-
-   loadMap(): void {
-     this.postionOptions = {
-       enableHighAccuracy: true,
-       timeout: 5000,
-       maximumAge: 0
-     };
-
-     this.geolocation.getCurrentPosition(this.postionOptions)
-     .then((pos) => {
-       console.log('Successfull');
-       const latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-       const mapOptions = {
-         center: latLng,
-         zoom: 15,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-       };
-
-       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-       this.addMarker(pos.coords.latitude, pos.coords.longitude);
-       this.nearbyPlace(latLng);
- })
-   .catch((error) => {
- console.log('Error getting location', error);
- });
-
-
-     // console.log('Successfull');
-     // const latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-     // const mapOptions = {
-     //   center: latLng,
-     //   zoom: 15,
-     //   mapTypeId: google.maps.MapTypeId.ROADMAP
-     // };
-     //
-     // this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-   }
-
-   addMarker(lat: number, lng: number): void {
-
-    const latLng = new google.maps.LatLng(lat, lng);
-
-    const marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: latLng,
-    icon: {
-      url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-    }
-    });
-    console.log('Hello');
-    marker.setMap(this.map);
-    // this.markers.push(marker);
-
-  }
- }
+       // const infowindow = new google.maps.InfoWindow();
+       // google.maps.event.addListener(this.markers, 'click', () => {
+       //   this.ngZone.run(() => {
+       //     infowindow.setContent(place.name);
+       //     infowindow.open(this.map, this.markers);
+       //   });
+       // });
+     } }
